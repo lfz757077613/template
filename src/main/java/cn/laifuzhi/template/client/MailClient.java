a505468aa5f4e81e6112baf366e5044fcc853773
fuzhi.lfz <fuzhi.lfz@alibaba-inc.com>
2021-11-01 21:00:46 +0800

    init

--- src/main/java/cn/laifuzhi/template/client/MailClient.java
+++ src/main/java/cn/laifuzhi/template/client/MailClient.java
@@ -22,8 +22,35 @@ public final class MailClient {
             message.setText("欢迎：" + content);
             //发送邮件
             sender.send(message);
+//            MimeMessage mimeMessage = sender.createMimeMessage();
+//            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+//            mimeMessageHelper.setFrom();
         } catch (Exception e) {
             log.error("send email error, content:{}", content, e);
         }
     }
+// 自定义javaMailSender
+//    public void sendDiv(String content) {
+//        try {
+//            JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+//            javaMailSender.setHost();
+////            默认-1，使用协议的默认port
+//            javaMailSender.setPort();
+//            javaMailSender.setUsername();
+//            javaMailSender.setPassword();
+//            javaMailSender.setProtocol("smtp");
+//            javaMailSender.setDefaultEncoding(StandardCharsets.UTF_8.name());
+//            javaMailSender.setJavaMailProperties();
+//
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setFrom("757077613@qq.com");
+//            message.setTo("757077613@qq.com");
+//            message.setSubject("新用户注册：" + content);
+//            message.setText("欢迎：" + content);
+//            //发送邮件
+//            javaMailSender.send(message);
+//        } catch (Exception e) {
+//            log.error("send email error, content:{}", content, e);
+//        }
+//    }
 }
