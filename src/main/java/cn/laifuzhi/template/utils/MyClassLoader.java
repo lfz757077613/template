5be6b098c646ed4c52ab5f363d9f99d6170b7633
fuzhi.lfz <fuzhi.lfz@alibaba-inc.com>
2021-11-10 10:58:00 +0800

    init

--- src/main/java/cn/laifuzhi/template/utils/MyClassLoader.java
+++ src/main/java/cn/laifuzhi/template/utils/MyClassLoader.java
@@ -2,17 +2,17 @@ package cn.laifuzhi.template.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Enumeration;
 
 @Slf4j
 public class MyClassLoader extends URLClassLoader {
     private ClassLoader javaseClassLoader;
 
-    public MyClassLoader(ClassLoader parent) {
-        super(new URL[0], parent);
+    public MyClassLoader() {
+        super(new URL[0], null);
         javaseClassLoader = getSystemClassLoader();
         while (javaseClassLoader != null && javaseClassLoader.getParent() != null) {
             javaseClassLoader = javaseClassLoader.getParent();
@@ -22,30 +22,43 @@ public class MyClassLoader extends URLClassLoader {
         }
     }
 
-    public void addFile(File file) {
-        try {
-            addURL(file.getCanonicalFile().toURI().toURL());
-        } catch (IOException e) {
-            log.error("addFile error", e);
-        }
-    }
-
     // 打破双亲委派模型，重写loadClass。反之重写findClass
     @Override
     protected Class<?> loadClass(String className, boolean resolve) throws ClassNotFoundException {
         synchronized (getClassLoadingLock(className)) {
-            Class<?> c = javaseClassLoader.loadClass(className);
+            Class<?> c = null;
+            try {
+                c = javaseClassLoader.loadClass(className);
+            } catch (ClassNotFoundException ignore){
+            }
             if (c != null) {
+                if (resolve) {
+                    resolveClass(c);
+                }
+                return c;
+            }
+            c = findLoadedClass(className);
+            if (c != null) {
+                if (resolve) {
+                    resolveClass(c);
+                }
                 return c;
             }
             c = findClass(className);
-            if (c == null) {
-                throw new ClassNotFoundException(className);
-            }
             if (resolve) {
                 resolveClass(c);
             }
             return c;
         }
+    }
+
+    @Override
+    public URL getResource(String name) {
+        return super.findResource(name);
+    }
+
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+        return super.findResources(name);
     }
 }
