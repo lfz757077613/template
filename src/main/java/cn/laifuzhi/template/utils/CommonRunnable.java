524a71c6d1711e230f90602cc17a7802dfeec176
fuzhi.lfz <fuzhi.lfz@alibaba-inc.com>
2021-11-15 12:18:30 +0800

    init

--- src/main/java/cn/laifuzhi/template/utils/CommonRunnable.java
+++ src/main/java/cn/laifuzhi/template/utils/CommonRunnable.java
@@ -4,14 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 public class CommonRunnable implements Runnable {
+    private final Thread curThread;
     private final Map<String, String> mdcMap;
     private final CommonContext commonContext;
     private final Runnable runnable;
 
     public CommonRunnable(Runnable runnable) {
+        this.curThread = Thread.currentThread();
         this.mdcMap = MDC.getCopyOfContextMap();
         this.commonContext = CommonContext.get();
         this.runnable = runnable;
@@ -23,15 +26,27 @@ public class CommonRunnable implements Runnable {
             if (commonContext != null) {
                 CommonContext.put(commonContext);
             }
-            if (mdcMap != null) {
-                MDC.setContextMap(mdcMap);
+            if (!Objects.equals(curThread, Thread.currentThread())) {
+                if (commonContext != null) {
+                    CommonContext.put(commonContext);
+                }
+                if (mdcMap != null) {
+                    MDC.setContextMap(mdcMap);
+                }
             }
             runnable.run();
         } catch (Throwable t) {
             log.error("CommonRunnable error", t);
+            throw t;
         } finally {
-            MDC.clear();
-            CommonContext.remove();
+            if (!Objects.equals(curThread, Thread.currentThread())) {
+                if (commonContext != null) {
+                    CommonContext.remove();
+                }
+                if (mdcMap != null) {
+                    MDC.clear();
+                }
+            }
         }
     }
 }
