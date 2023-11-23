1c2c3b267f200a1ea0d483d8902f73342e47bdf6
fuzhi.lfz <fuzhi.lfz@alibaba-inc.com>
2023-11-24 00:10:55 +0800

    优化appctl脚本

--- bin/appctl.sh
+++ bin/appctl.sh
@@ -2,7 +2,7 @@
 
 usage ()
 {
-    echo "Usage: $0 {start|stop|online|offline|restart}"
+    echo "Usage: $0 {start|start_k8s|stop|online|offline|restart}"
     exit 1
 }
 
@@ -33,70 +33,71 @@ start ()
     for e in $(seq 90); do
         echo "check http://127.0.0.1:${SERVER_PORT}/${HEALTH_CHECK_PATH} ${e}"
         sleep 1
-        status_code=`/usr/bin/curl -L -o /dev/null --connect-timeout 1 -s -w %{http_code} "http://127.0.0.1:${SERVER_PORT}/${HEALTH_CHECK_PATH}"`
+        status_code=`/usr/bin/curl -L -o /dev/null --connect-timeout 1 -m 1 -s -w %{http_code} "http://127.0.0.1:${SERVER_PORT}/${HEALTH_CHECK_PATH}"`
         if [ ${status_code} == 200 ];then
             echo "check http://127.0.0.1:${SERVER_PORT}/${HEALTH_CHECK_PATH} success"
             return 0
         fi
     done
     echo "check http://127.0.0.1:${SERVER_PORT}/${HEALTH_CHECK_PATH} fail"
-    return 1 # status.taobao check failed
+    return 1
 }
 
 stop()
 {
     offline
-    for e in $(seq 20); do
+    for e in `seq 20`; do
         echo "wait offline ${e} ..."
         sleep 1
     done
-
-    STR=`ps -ef | grep java | grep "$APP_NAME"`
-    [ -z "$STR" ] && return 0
-    kill `echo "$STR" | awk '{print $2}'`
-    while true
-    do
-        STR=`ps -ef | grep java | grep "$APP_NAME"`
-        [ -z "$STR" ] && break || sleep 1
+    STR=`ps -ef | grep java | grep ${APP_NAME}`
+    [ -z ${STR} ] && return 0
+    kill `echo ${STR} | awk '{print $2}'`
+    while true; do
+        STR=`ps -ef | grep java | grep ${APP_NAME}`
+        [[ -z ${STR} ]] && break || sleep 1
         echo "wait kill ..."
     done
+    echo "stopped"
 }
 
 online()
 {
-    touch -m $APP_HOME/${HEALTH_CHECK_FILE}
+    touch -m ${APP_HOME}/${HEALTH_CHECK_FILE}
 }
 
 offline()
 {
-    rm -f $APP_HOME/${HEALTH_CHECK_FILE}
+    rm -f ${APP_HOME}/${HEALTH_CHECK_FILE}
 }
 
 case $1 in
-
     start)
         start
     ;;
-
+    start_k8s)
+        trap "stop; exit" SIGTERM SIGINT
+        start
+        if [ $? -eq 0 ]; then
+            while true; do
+                sleep 1;
+            done
+        fi
+    ;;
     stop)
         stop
     ;;
-
     restart)
         stop
         start
     ;;
-
     online)
         online
     ;;
-
     offline)
         offline
     ;;
-
     *)
         usage
     ;;
-
 esac
